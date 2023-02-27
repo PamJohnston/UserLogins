8172ef04496cfa0eb4995bb898c133314a719956
PamJohnston <p.johnston2@rgu.ac.uk>
2023-02-27 14:47:57 +0000

    some code in the server

--- Login_BackEnd/index.js
+++ Login_BackEnd/index.js
@@ -3,11 +3,15 @@ var express = require('express');
 var app = express();
 const PORT = 8000
 
+
+
+
+
+
 // 404 page
 app.use(function ( req, res, next) {
     res.send('This page does not exist!')
 });
-
 
 app.listen(PORT, function () {
     console.log('Listening on http://localhost:'+PORT+'/');
