6e31feba780c58af75858e42dd14db10a1531c56
PamJohnston <p.johnston2@rgu.ac.uk>
2023-02-27 15:48:37 +0000

    database added

--- Login_BackEnd/index.js
+++ Login_BackEnd/index.js
@@ -1,8 +1,20 @@
 var path = require('path');
 var express = require('express');
-var app = express();
-const PORT = 8000
 
+var app = express();
+const PORT = 8000;
+
+
+// database stuff
+// Connection URL - you should set this with a configuration 
+//   file that doesn't go in GitHub, though
+const MONGOURI = "mongodb://127.0.0.1:27017";
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise
+mongoose.connect(MONGOURI, { dbName: "users" })
+mongoose.connection.on('error', err => {
+  throw new Error(`unable to connect to database: ${MONGOURI}`)
+})
 
 
 
