6c14f9399ea435f6260c9c23e68dff5dd124b916
PamJohnston <p.johnston2@rgu.ac.uk>
2023-02-27 18:46:38 +0000

    Insecure user login working, refactored index.js for neatness

--- Login_BackEnd/index.js
+++ Login_BackEnd/index.js
@@ -1,9 +1,8 @@
 import path from 'path';
 import express from 'express';
 import userRoutes from './user.routes.js'
-
-
 import bodyParser from 'body-parser'
+import mongoose from 'mongoose';
 
 var app = express();
 
@@ -11,16 +10,6 @@ import config from './config.js';
 const PORT = config.port;
 const MONGOURI = config.mongoUri;
 
-
-// database stuff
-// Connection URL - you should set this with a configuration 
-//   file that doesn't go in GitHub, though
-import mongoose from 'mongoose';
-//mongoose.Promise = global.Promise
-//mongoose.connect(MONGOURI, { dbName: "users" })
-//mongoose.connection.on('error', err => {
-//  throw new Error(`unable to connect to database: ${MONGOURI}`)
-//})
 await mongoose.connect('mongodb://127.0.0.1/my_database');
 
 
