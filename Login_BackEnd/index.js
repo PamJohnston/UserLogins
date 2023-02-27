6657f1f23fda7d5dbc276c5251787483dccede7d
PamJohnston <p.johnston2@rgu.ac.uk>
2023-02-27 20:15:25 +0000

    Security and middleware added, untested

--- Login_BackEnd/index.js
+++ Login_BackEnd/index.js
@@ -3,13 +3,14 @@ import express from 'express';
 import userRoutes from './user.routes.js'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose';
+import authRoutes from './auth.routes.js'
 
 var app = express();
 
 import config from './config.js';
 const PORT = config.port;
 const MONGOURI = config.mongoUri;
-
+// The database connection (just crash without it)
 await mongoose.connect('mongodb://127.0.0.1/my_database');
 
 
@@ -17,7 +18,19 @@ await mongoose.connect('mongodb://127.0.0.1/my_database');
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
+app.use('/', authRoutes)
 app.use('/', userRoutes)
+
+// Authorisation error
+app.use((err, req, res, next) => {
+    if (err.name === 'UnauthorizedError') {
+      res.status(401).json({"error" : err.name + ": " + err.message})
+    }else if (err) {
+      res.status(400).json({"error" : err.name + ": " + err.message})
+      console.log(err)
+    }
+  })
+  
 
 // 404 page
 app.use(function ( req, res, next) {
--- Login_BackEnd/user.routes.js
+++ Login_BackEnd/user.routes.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import userCtrl from './user.controller.js'
+import authCtrl from './auth.controller.js'
 
 const router = express.Router()
 
@@ -7,11 +8,17 @@ router.route('/api/users')
   .get(userCtrl.list)
   .post(userCtrl.create)
 
-router.route('/api/users/:userId')
-  .get(userCtrl.read)
-  .put(userCtrl.update)
-  .delete(userCtrl.remove)
+//router.route('/api/users/:userId')
+//  .get(userCtrl.read)
+//  .put(userCtrl.update)
+//  .delete(userCtrl.remove)
 
+router.route('/api/users/:userId')
+  .get(authCtrl.requireSignin, authCtrl.hasAuthorization, userCtrl.read)
+  .put(authCtrl.requireSignin, authCtrl.hasAuthorization, userCtrl.update)
+  .delete(authCtrl.requireSignin, authCtrl.hasAuthorization, userCtrl.remove)
+
+/// Leaving this one insecure for now...
 router.route('/api/userbyname/:userName')
   .get(userCtrl.read)
   .put(userCtrl.update)
